30ecd4c54eac1581ebbc71f05cbedc9792aa1749
itzero-ru <paw100d@gmail.com>
2024-11-14 11:50:21 +0300

    removed unused routers

--- fastapi_async_sql_profiler/routers.py
+++ fastapi_async_sql_profiler/routers.py
@@ -1,29 +1,17 @@
-import os
-import math
-from pathlib import Path
-from typing import Annotated, Literal, Union
+from typing import Literal
 from fastapi import APIRouter, Depends, Query, Request, Response, status
-from fastapi.responses import HTMLResponse, JSONResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-from fastapi_async_sql_profiler.database import async_session_maker, clear_table_bd
+from fastapi_async_sql_profiler.database import clear_table_bd
 
 from fastapi_async_sql_profiler.config import APP_ROUTER_PREFIX
 from fastapi_async_sql_profiler.dependencies import get_query_info_service, get_request_info_service
-from fastapi_async_sql_profiler.repository import RequestInfoRepository
 from fastapi_async_sql_profiler.schemas.common_schemas import PaginationMeta, PaginationResponse
 from fastapi_async_sql_profiler.schemas.query_info_schema import QueryInfoDetail
-from fastapi_async_sql_profiler.schemas.request_info_schema import RequestInfoDetail, RequestInfoDetailForList, RequestInfoList
-from fastapi_async_sql_profiler.schemas.response_info_schema import ResponseInfoDetail
+from fastapi_async_sql_profiler.schemas.request_info_schema import RequestInfoDetail, RequestInfoDetailForList
 from fastapi_async_sql_profiler.services import QueryInfoService, RequestInfoService
-
 from .models import Items, QueryInfo, RequestInfo, ResponseInfo
-from .crud import add_db, add_one, filter_obj, get_obj_by_id, get_requests_with_query_count
-
+from .crud import add_one
 from .pages import router as router_pages
-
-#
-from fastapi import Header, HTTPException
+# from fastapi import Header, HTTPException
 
 
 router = APIRouter(
@@ -33,7 +21,7 @@ router = APIRouter(
 # static_files = StaticFiles(directory="static")
 # router.mount("/static", static_files, name="static")
 
-# Добавляем роуты из другого файла
+# Adding routes from another file
 router.include_router(router_pages)
 
 
@@ -58,25 +46,11 @@ async def requests_show(
     request: Request,
     request_info_service: RequestInfoService = Depends(
         get_request_info_service),
-    #
-    # user_agent: Annotated[Union[str, None], Header()] = None,
-    # user_agent: str | None = Header(default=None),
-    # authorization: str = Header(default=None),
-    # q: Annotated[Union[str, None], Query(max_length=50)] = None
     page: int = Query(1, gt=0),
     size: int = Query(10, gt=0),
     order_by: Literal['ASC', 'DESC'] = 'DESC'
 ):
     """Get all requests."""
-
-    # order_dir = {
-    #     'ASC': {
-    #         'label': 'Ascending'
-    #     },
-    #     'DESC': {
-    #         'label': 'Descending'
-    #     }
-    # }
 
     all_items = await request_info_service.get_request_info_all(page=page, size=size, order_by=order_by)
 
@@ -117,13 +91,6 @@ async def request_show(
 ):
     """Get single request."""
 
-    # def convert_to_dict(orm_object):
-    #     return {
-    #         field: getattr(
-    #             orm_object, field) for field in orm_object.__dict__ if not field.startswith('_')}
-
-    # request_query = await get_obj_by_id(
-    #     RequestInfo, id, joinedload_names=['response_info',])
     request_query = await request_info_service.get_request_info_by_id(id)
 
     if not request_query:
@@ -159,64 +126,3 @@ async def post_item(request: Request, response: Response):
     await add_one(Items, {'body': '55555'})
     response.status_code = status.HTTP_201_CREATED
     return {"Hello": "World"}
-
-
-@router.get("/request_detail_one/{id}")
-async def request_one_show(
-    id: int, request: Request,
-    # response_model_exclude_none=True,
-):
-    """Get single request."""
-
-    async with async_session_maker() as session:
-        service = RequestInfoService(RequestInfoRepository(session))
-        request_query = await service.get_request_info_by_id(id)
-
-    # request_query = await get_obj_by_id(
-    #     RequestInfo, id, joinedload_names=['response_info',])
-    if not request_query:
-        return Response(status_code=status.HTTP_404_NOT_FOUND)
-    # return request_query
-    request_query_validated_data = RequestInfoDetail.model_validate(
-        request_query)
-    return request_query_validated_data
-    # request_query_validated_data = RequestInfoDetail.from_orm(
-    #     request_query)
-
-    query_detail = await filter_obj(QueryInfo, request_id=id)
-    query_validated_data = [
-        QueryInfoDetail.model_validate(item) for item in query_detail]
-
-    # return {'ok': {'request': request_query_validated_data, 'query': query_validated_data}}
-
-    sum_on_query = 0
-    for query_details in query_detail:
-        sum_on_query = sum_on_query + query_details.time_taken
-
-    context = {
-        "current_id": id,
-        # "request": request,
-        "request_query": request_query_validated_data,
-        "query_details": query_validated_data,
-        "sum_on_query": sum_on_query,
-    }
-    return context
-
-# ggg
-
-
-@router.get("/response_detail_one/{id}")
-async def response_one_show(
-    id: int, request: Request,
-    # response_model_exclude_none=True,
-):
-    """Get single request."""
-
-    request_query = await get_obj_by_id(
-        ResponseInfo, id,)
-    if not request_query:
-        return Response(status_code=status.HTTP_404_NOT_FOUND)
-    # return request_query
-    validated_data = ResponseInfoDetail.model_validate(
-        request_query)
-    return validated_data
